da8c70725578255daeffb0962fe521254999026a
jinlong <jinlong4696@163.com>
2024-03-07 00:43:07 +0800

    generics test name

--- generics/add_test.go
+++ generics/add_test.go
@@ -7,5 +7,5 @@ import (
 func TestAdd(t *testing.T) {
 	t.Log(Add[int](1, 2))
 	t.Log(Add(2, 2))
-	t.Log(Add(1.1, 2.2))
+	t.Logf("%f",Add(1.1, 2.2))
 }
--- generics/constraints_test.go
+++ generics/constraints_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestAdd3(t *testing.T) {
+func TestConstraints(t *testing.T) {
 	t.Log(Add3(1.2, 2.3))
 	var a, b complex64 = 1.2, 2.3
 	t.Log(Add3(a, b))
