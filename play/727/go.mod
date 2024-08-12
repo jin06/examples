e6da70949274f615d60902dd31b49b1a455f18cb
jinlong <jlonmyway@gmail.com>
2024-08-12 09:35:24 +0800

    play

---
+++ play/727/go.mod
@@ -0,0 +1,3 @@
+module example.com/play727
+
+go 1.22.1
---
+++ play/727/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type T struct {
+	Field1 int32
+	Field2 int32
+}
+
+type T2 struct {
+	X     [1 << 24]byte
+	Field int32
+}
+
+func main() {
+	var x *T
+	p1 := &x.Field1
+	p2 := &x.Field2
+	var x2 *T2
+	p3 := &x2.Field
+	fmt.Println(max(1, 2, 3))
+	fmt.Println(p1, p2, p3)
+}
