b66c80cf4b6e3cfb935b7bbec939fad05c76e299
jinlong <jlonmyway@gmail.com>
2024-08-14 17:59:27 +0800

    forrange

--- forrange/main.go
+++ forrange/main.go
@@ -2,18 +2,40 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
 func main() {
 	values := []int{1, 2, 3}
-	wg := sync.WaitGroup{}
 	for _, v := range values {
-		wg.Add(1)
 		go func() {
 			fmt.Println(v)
-			wg.Done()
 		}()
 	}
-	wg.Wait()
+	<-time.Tick(time.Second)
+	oldRange()
+	<-time.Tick(time.Second)
+	newRange()
+	<-time.Tick(time.Second)
+}
+
+func oldRange() {
+	values := []int{1, 2, 3}
+	var rec int
+	for _, v := range values {
+		rec = v
+		go func() {
+			fmt.Println(rec)
+		}()
+	}
+}
+
+func newRange() {
+	values := []int{1, 2, 3}
+	for _, v := range values {
+		var rec = v
+		go func() {
+			fmt.Println(rec)
+		}()
+	}
 }
