8294e19a016d58c7dde37bf5e93c5d511fcf3184
jl <jlonmyway@gmail.com>
2024-08-22 00:05:11 +0800

    foreach

--- forrange/main.go
+++ forrange/main.go
@@ -8,34 +8,10 @@ import (
 func main() {
 	values := []int{1, 2, 3}
 	for _, v := range values {
+		v := v
 		go func() {
 			fmt.Println(v)
 		}()
 	}
 	<-time.Tick(time.Second)
-	oldRange()
-	<-time.Tick(time.Second)
-	newRange()
-	<-time.Tick(time.Second)
-}
-
-func oldRange() {
-	values := []int{1, 2, 3}
-	var rec int
-	for _, v := range values {
-		rec = v
-		go func() {
-			fmt.Println(rec)
-		}()
-	}
-}
-
-func newRange() {
-	values := []int{1, 2, 3}
-	for _, v := range values {
-		var rec = v
-		go func() {
-			fmt.Println(rec)
-		}()
-	}
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.20
 
 require golang.org/x/exp v0.0.0-20240222234643-814bf88cf225
 
-require golang.org/dl v0.0.0-20240621154342-20a4bcbb3ee2 // indirect
+require golang.org/dl v0.0.0-20240813161640-304e16060ce9 // indirect
